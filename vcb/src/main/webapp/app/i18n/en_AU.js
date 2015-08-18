57ec6e32d37dd46340c5aaef57b7c478dc4f78ce
asamgir <ajaysamgir@gmail.com>
2015-08-18 16:00:46 +0530

    Added VCB plugin in os-extensions
    Fixed : SUPPORT-542 "Specimen label" is required to be changed as "Tissue Bank Number"
    1.Replace specimen label to Tissue Bank Number from following pages
     - CP add/edit
     - Events (CP level)
     - Distributions
     - Specimen collection
     - Specimen
     - Visit
     - Visit participant
    2.Replaced from related error messages.

---
+++ vcb/src/main/webapp/app/i18n/en_AU.js
@@ -0,0 +1,17 @@
+{
+  "specimens": {
+    "label": "Tissue Bank Number"
+  },
+
+  "cp": {
+    "spec_labels": "Tissue Bank Numbers"  
+  },
+
+  "orders": {
+    "enter_specimen_label": "Enter tissue bank numbers separated by comma/tab/newline",
+
+    "spec": {
+      "label": "Tissue Bank Number"
+    }
+  }
+}
