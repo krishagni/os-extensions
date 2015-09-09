337c0e0dd8d4b5a29f3130e13458d0ab203b846a
Vinayak Pawar <vinayakapawar@gmail.com>
2015-09-09 11:29:54 +0530

    OPSMN-2441: Use explicit providers to register plugin views and templates. Store plugin access button/link templates into separate HTML files

--- sgh/src/main/webapp/app/module.js
+++ sgh/src/main/webapp/app/module.js
@@ -25,38 +25,7 @@ osApp.providers
   });
 
   function initSghPlugin() {
-    var injector = angular.injector(['ng', 'openspecimen']);
-    var tmplCache = injector.get('$templateCache');
-    var pluginReg = injector.get('PluginReg');
-
-    tmplCache.put(
-      'plugin-ui-resources/sgh/cp-bulk-reg-btn.html',
-      '<span ng-controller="sgh.BulkRegistrationButtonCtrl">'+
-      '  <button class="default" ui-sref="cp-bulk-part-regs" ng-if="bulkRegEnabled">' +
-      '    <span class="fa fa-plus"></span>' +
-      '    <span translate="custom_sgh.bulk_reg">' +
-      '      Bulk Register' +
-      '    </span>' +
-      '  </button>' +
-      '</span>'
-    );
-
-    tmplCache.put(
-      'plugin-ui-resources/sgh/unplanned-bulk-pre-print-icon.html',
-      '<li class="os-home-item"> ' +
-        '<a ui-sref="unplanned-bulk-pre-printing">' +
-          '<span class="os-home-item-icon">' +
-            '<span class="fa fa-print"></span>' +
-          '</span>' +
-          '<span class="os-home-item-info">' +
-            '<h3 translate="custom_sgh.bulk_pre_printing">Bulk Pre Print TRIDs</h3>' +
-            '<span translate="custom_sgh.bpp_desc">Bulk pre print TRIDs for unplanned collection protocols</span>' +
-          '</span>' +
-        '</a>' +
-      '</li> '
-    );
-      
-    pluginReg.registerViews(
+    osApp.providers.pluginReg.registerViews(
       'sgh',
       {
         'participant-list': {
