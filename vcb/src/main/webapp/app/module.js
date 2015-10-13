2c38ca7996028d226ff30b3c5952a1b17ff0cc63
vinaylonushte <vinayak.lonushte@krishagni>
2015-10-13 13:00:18 +0530

    SUPPORT-603: Hid race field from participant addedit and overview page.

--- vcb/src/main/webapp/app/module.js
+++ vcb/src/main/webapp/app/module.js
@@ -2,6 +2,18 @@ function initVcbPlugin() {
   osApp.providers.pluginReg.registerViews(
     'vcb',
     {
+      'participant-addedit': {
+        'page-body': {
+          template: 'plugin-ui-resources/vcb/participant-addedit.html'
+        }
+      },
+
+      'participant-detail': {
+        'overview': {
+          template: 'plugin-ui-resources/vcb/participant-overview.html'
+        }
+      },
+
       'visit-addedit': {
         'page-body': {
           template: 'plugin-ui-resources/vcb/addedit-visit.html'
