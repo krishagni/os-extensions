0d1af1c0163d4294c39c671e05efb1992f5d3863
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2015-12-10 14:31:50 +0530

    Missed file for last check-in for #SUPPORT-813

--- sgh/src/main/java/com/krishagni/openspecimen/sgh/events/BulkTridPrintOpDetail.java
+++ sgh/src/main/java/com/krishagni/openspecimen/sgh/events/BulkTridPrintOpDetail.java
@@ -1,11 +1,15 @@
 package com.krishagni.openspecimen.sgh.events;
 
+import javax.print.attribute.standard.PrinterLocation;
+
 
 public class BulkTridPrintOpDetail {
 
 	private Integer tridCount;
 	
 	private String printerName;
+	
+	private Boolean printLabels;
 	
 	public Integer getTridCount() {
 		return tridCount;
@@ -21,6 +25,14 @@ public class BulkTridPrintOpDetail {
 
 	public void setPrinterName(String printerName) {
 		this.printerName = printerName;
+	}
+
+	public Boolean isPrintLabels() {
+		return printLabels == null ? false : printLabels;
+	}
+
+	public void setPrintLabels(Boolean printLabels) {
+		this.printLabels = printLabels;
 	}
 	
 }
