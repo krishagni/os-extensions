6e27e2d5ce2fac22b95ecc676583858cce9f9514
Satish Lakhani <satish.lakhani@krishagni.com>
2015-12-11 09:59:42 +0530

    SGH: Added missing i18n keys for configuration properties

--- sgh/src/main/webapp/app/i18n/en_SG.js
+++ sgh/src/main/webapp/app/i18n/en_SG.js
@@ -52,5 +52,23 @@
   "dp": {
    "title": "Title",
    "short_title": "TR Code"
+  },
+  
+  "settings": {
+    "plugin_sgh": {
+      "title": "Custom Module",
+      "copies_to_print": "Copies to print",
+      "copies_to_print_desc": "Number of Copies to print",
+      "malignant_aliq_cnt": "Malignant aliquot count",
+      "malignant_aliq_cnt_desc": "Number of malignant aliquot labels to print for unplanned protocol",
+      "malignant_aliq_suffix": "Malignant aliquot suffix",
+      "malignant_aliq_suffix_desc": "Suffix for malignant aliquot label",
+      "non_malignant_aliq_cnt": "Non-Malignant aliquot count",
+      "non_malignant_aliq_cnt_desc": "Number of non-malignant aliquot labels to print for unplanned protocol",
+      "non_malignant_aliq_suffix": "Non-Malignant aliquote suffix",
+      "non_malignant_aliq_suffix_desc": "Suffix for non-malignant aliquote",
+      "trid_copies": "Trid copies to print",
+      "trid_copies_desc": "Trid copies to print for planned or unplanned protocols"
+    }
   }
 }
