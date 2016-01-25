1eb99062b735af73ab5319af7748083f10bd9b02
Satish Lakhani <satish.lakhani@krishagni.com>
2016-01-25 19:11:21 +0530

    Done indentation changes

--- asig/src/main/webapp/app/module.js
+++ asig/src/main/webapp/app/module.js
@@ -3,47 +3,47 @@ angular.module('os.plugins.svh', ['openspecimen'])
     PluginReg.registerViews(
       'svh',
       {
-		    'participant-list': {
-		      'header': {
-		        template: 'plugin-ui-resources/svh/participant-list-header.html'
-		      }
-		    },
-		    
-		    'participant-detail': {
-		      'summary': {
-		        template: 'plugin-ui-resources/svh/participant-summary.html'
-		      }
-		    },
-		    
-		    'participant-addedit': {
-		      'page-body': {
-		        template: 'plugin-ui-resources/svh/participant-edit.html'
-		      }
-		    },
-		    
-		    'specimen-detail': {
-		      'overview': {
-		        template: 'plugin-ui-resources/svh/specimen/overview.html'
-		      }
-		    },
-		    
-		    'specimen-addedit': {
-		      'page-body': {
-		        template: 'plugin-ui-resources/svh/specimen/addedit.html'
-		      }
-		    },
-		    
-		    'specimen-create-derivative': {
-		      'page-body': {
-		        template: 'plugin-ui-resources/svh/specimen/add-derivative.html'
-		      }
-		    },
-		    
-		    'specimen-create-aliquots': {
-		      'page-body': {
-		        template: 'plugin-ui-resources/svh/specimen/add-aliquots.html'
-		      }
-		    }
-		  }
-		);
+        'participant-list': {
+          'header': {
+            template: 'plugin-ui-resources/svh/participant-list-header.html'
+          }
+        },
+        
+        'participant-detail': {
+          'summary': {
+            template: 'plugin-ui-resources/svh/participant-summary.html'
+          }
+        },
+        
+        'participant-addedit': {
+          'page-body': {
+            template: 'plugin-ui-resources/svh/participant-edit.html'
+          }
+        },
+        
+        'specimen-detail': {
+          'overview': {
+            template: 'plugin-ui-resources/svh/specimen/overview.html'
+          }
+        },
+        
+        'specimen-addedit': {
+          'page-body': {
+            template: 'plugin-ui-resources/svh/specimen/addedit.html'
+          }
+        },
+        
+        'specimen-create-derivative': {
+          'page-body': {
+            template: 'plugin-ui-resources/svh/specimen/add-derivative.html'
+          }
+        },
+        
+        'specimen-create-aliquots': {
+          'page-body': {
+            template: 'plugin-ui-resources/svh/specimen/add-aliquots.html'
+          }
+        }
+      }
+    );
   });
