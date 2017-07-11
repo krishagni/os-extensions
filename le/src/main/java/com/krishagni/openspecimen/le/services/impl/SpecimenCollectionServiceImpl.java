f0f19aa7d68ff2a33ab3fe76f591cfa8be026066
Richard Bramley <rabramley@gmail.com>
2017-07-11 09:50:44 +0100

    v3.4 fixes

--- le/src/main/java/com/krishagni/openspecimen/le/services/impl/SpecimenCollectionServiceImpl.java
+++ le/src/main/java/com/krishagni/openspecimen/le/services/impl/SpecimenCollectionServiceImpl.java
@@ -24,21 +24,21 @@ import com.krishagni.openspecimen.le.events.SpecimenAndFrozenEventDetail.EventDe
 import com.krishagni.openspecimen.le.services.SpecimenCollectionService;
 
 public class SpecimenCollectionServiceImpl implements SpecimenCollectionService {
-	
+
 	private VisitService visitService;
-	
+
 	private SpecimenService specimenSvc;
-	
+
 	private ConfigurationService cfgSvc;
-	
+
 	public void setVisitService(VisitService visitService) {
 		this.visitService = visitService;
 	}
-	
+
 	public void setSpecimenSvc(SpecimenService specimenSvc) {
 		this.specimenSvc = specimenSvc;
 	}
-	
+
 	public void setCfgSvc(ConfigurationService cfgSvc) {
 		this.cfgSvc = cfgSvc;
 	}
@@ -47,67 +47,67 @@ public class SpecimenCollectionServiceImpl implements SpecimenCollectionService 
 	@PlusTransactional
 	public ResponseEvent<List<VisitSpecimenDetail>> collectVisitsAndSpecimens(RequestEvent<List<VisitSpecimenDetail>> req) {
 		List<VisitSpecimenDetail> responses = new ArrayList<VisitSpecimenDetail>();
-		
+
 		for (VisitSpecimenDetail detail : req.getPayload()) {
 			RequestEvent<VisitSpecimenDetail> subReq = new RequestEvent<VisitSpecimenDetail>(detail);
 			ResponseEvent<VisitSpecimenDetail> subResp = visitService.collectVisitAndSpecimens(subReq);
 			if (!subResp.isSuccessful()) {
 				return ResponseEvent.error(subResp.getError());
 			}
-						
+
 			responses.add(subResp.getPayload());
 		}
-		
+
 		return new ResponseEvent<List<VisitSpecimenDetail>>(responses);
 	}
-	
+
 	@Override
 	@PlusTransactional
 	public ResponseEvent<SpecimenAndFrozenEventDetail> collectChildSpecimens(RequestEvent<SpecimenAndFrozenEventDetail> req) {
-		try {		
+		try {
 			SpecimenAndFrozenEventDetail detail = req.getPayload();
-			
+
 			//
 			// Step 1: collect child specimens
 			//
 			RequestEvent<List<SpecimenDetail>> collectChildSpmnsReq = new RequestEvent<List<SpecimenDetail>>(detail.getSpecimens());
 			ResponseEvent<List<SpecimenDetail>> collectChildSpmnsResp = specimenSvc.collectSpecimens(collectChildSpmnsReq);
 			collectChildSpmnsResp.throwErrorIfUnsuccessful();
-	
+
 			//
 			// Step 2: add frozen events
 			//
 			addFrozenEvents(getReqSpecimenIdMap(collectChildSpmnsResp.getPayload()), detail.getEvents());
-			
+
 			//
 			// Step 3: construct response
-			//			
+			//
 			SpecimenAndFrozenEventDetail respDetail = new SpecimenAndFrozenEventDetail();
 			respDetail.setSpecimens(collectChildSpmnsResp.getPayload());
 			respDetail.setEvents(detail.getEvents());
-			return ResponseEvent.response(respDetail);			
+			return ResponseEvent.response(respDetail);
 		} catch (OpenSpecimenException ose) {
 			return ResponseEvent.error(ose);
 		} catch (Exception e) {
 			return ResponseEvent.serverError(e);
 		}
 	}
-	
+
 	private Map<String, Long> getReqSpecimenIdMap(List<SpecimenDetail> specimens) {
 		Map<String, Long> result = new HashMap<String, Long>();
-		
+
 		if (specimens == null) {
 			return result;
 		}
-		
+
 		for (SpecimenDetail specimen : specimens) {
 			result.put(specimen.getVisitId() + "-" + specimen.getReqId(), specimen.getId());
-			result.putAll(getReqSpecimenIdMap(specimen.getChildren()));			
+			result.putAll(getReqSpecimenIdMap(specimen.getChildren()));
 		}
-		
+
 		return result;
 	}
-	
+
 	private void addFrozenEvents(Map<String, Long> reqSpecimenIdMap, List<EventDetail> events) {
 		for (EventDetail event : events) {
 			Long specimenId = event.getSpecimenId();
@@ -119,25 +119,26 @@ public class SpecimenCollectionServiceImpl implements SpecimenCollectionService 
 			if (specimenId == null) {
 				continue;
 			}
-			
+
 			Long userId = AuthUtil.getCurrentUser().getId();
 			if (event.getUser() != null && event.getUser().getId() != null) {
 				userId = event.getUser().getId();
 			}
-			
+
 			Date time = Calendar.getInstance().getTime();
 			if (event.getTime() != null) {
 				time = event.getTime();
 			}
-			
+
 			event.setId(addFrozenEvent(specimenId, userId, time));
 		}
 	}
-		
+
 	private Long addFrozenEvent(Long specimenId, Long userId, Date time) {
 		Map<String, Object> values = new HashMap<String, Object>();
 		values.put("user", userId);
-		values.put("time", new SimpleDateFormat(cfgSvc.getDeDateTimeFormat()).format(time));		
-		return DeObject.saveFormData("SpecimenFrozenEvent", "SpecimenEvent", -1L, specimenId, values);		
+		values.put("time", new SimpleDateFormat(cfgSvc.getDeDateTimeFormat()).format(time));
+		values.put("frozenMethod", "Not Specified");
+		return DeObject.saveFormData("SpecimenFrozenEvent", "SpecimenEvent", -1L, specimenId, values);
 	}
 }
