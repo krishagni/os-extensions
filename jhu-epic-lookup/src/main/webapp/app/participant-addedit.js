305e0a6a57a401fe14014855f56ab4bb6084ec7d
Vinayak Pawar <vinayakapawar@gmail.com>
2018-02-13 09:08:15 +0530

    OPSMN-3898, SUPPORT-2692: Make the JHU specific participant matching logic compatible with v4.2+ lookup workflow.

--- jhu-epic-lookup/src/main/webapp/app/participant-addedit.js
+++ jhu-epic-lookup/src/main/webapp/app/participant-addedit.js
@@ -1,45 +1,66 @@
 angular.module('os.plugins.jhu-epic-lookup')
   .controller('jhuEpicParticipantAddEditCtrl',
     function($scope, $interval, $document, Participant, jhuEpicParticipantUtil) {
-      $scope.partCtx.includeSiteTypes = ['EPIC'];
 
-      var participant = $scope.cpr.participant;
-      jhuEpicParticipantUtil.addMatchParticipantsFn(participant);
+      var hideFn;
 
-      var hideFn = $interval(
-        function() {
-          //
-          // UID field is not used
-          //
-          var uidInputEl = $document.find('input[name="uid"]');
-          uidInputEl.parent().parent().hide();
+      function init() {
+        $scope.partCtx.includeSiteTypes = ['EPIC'];
 
-          if (!$scope.partCtx.twoStep) {
-            //
-            // Hide MPI field in regular workflow for participants
-            // not sourced from EPIC
-            //
-            if ($scope.cpr.participant.source != 'EPIC' && !$scope.cpr.participant.id) {
-              var empiInputEl = $document.find('input[name="empi"]');
-              empiInputEl.parent().parent().hide();
+        var participant = $scope.cpr.participant;
+        jhuEpicParticipantUtil.addMatchParticipantsFn(participant);
+        hideFn = $interval(hideParticipantFields, 100, 0, false);
+
+        $scope.$watch('partCtx.step',
+          function(newVal, oldVal) {
+            if (!oldVal) {
+              return;
+            }
+
+            delete $scope.cpr.participant.$$epicMatchingFn
+            jhuEpicParticipantUtil.addMatchParticipantsFn($scope.cpr.participant);
+            if (!hideFn) {
+              hideFn = $interval(hideParticipantFields, 100, 0, false);
             }
           }
+        );
+      }
 
+      function hideParticipantFields() {
+        //
+        // UID field is not used
+        //
+        var uidInputEl = $document.find('input[name="uid"]');
+        uidInputEl.parent().parent().hide();
+
+        if (!$scope.partCtx.twoStep) {
           //
-          // No MRNs for non-EPIC participants
+          // Hide MPI field in regular workflow for participants
+          // not sourced from EPIC
           //
           if ($scope.cpr.participant.source != 'EPIC' && !$scope.cpr.participant.id) {
-            var pmiInputEl = $document.find('div[id="pmiForm"]');
-            pmiInputEl.hide();
+            var empiInputEl = $document.find('input[name="empi"]');
+            empiInputEl.parent().parent().hide();
           }
+        }
 
-          if (uidInputEl.length > 0) {
-            //
-            // Cancel only when we are sure all required fields are hidden
-            //
-            $interval.cancel(hideFn);
-          }
-        }, 100, 0, false);
+        //
+        // No MRNs for non-EPIC participants
+        //
+        if ($scope.cpr.participant.source != 'EPIC' && !$scope.cpr.participant.id) {
+          var pmiInputEl = $document.find('div[id="pmiForm"]');
+          pmiInputEl.hide();
+        }
 
+        if (uidInputEl.length > 0) {
+          //
+          // Cancel only when we are sure all required fields are hidden
+          //
+          $interval.cancel(hideFn);
+          hideFn = null;
+        }
+      }
+
+      init();
     }
   );
