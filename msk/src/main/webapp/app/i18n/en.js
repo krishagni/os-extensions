74ef686e76f0c225a5e92e19d3b07a9805536162
Vinayak Pawar <vinayakapawar@gmail.com>
2018-05-22 15:39:10 +0530

    OPSMN-4344: MSKCC specific UI strings.
    For examples, the visit status "Complete" is displayed as "Banked".
    Search Visits is displayed as "Search Visits (HL7)"
    Pathology status is displayed as "Category"

---
+++ msk/src/main/webapp/app/i18n/en.js
@@ -0,0 +1,16 @@
+{
+  "specimens": {
+    "pathology": "Category",
+    "pathology_status": "Category"
+  },
+
+  "sde": {
+    "search_visits": "Search Visits (HL7)"
+  },
+
+  "visits": {
+    "status": {
+      "complete": "Banked"
+    }
+  }
+}
