9cc790c1557ad5b1c81be9a5b75981364b841256
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2020-01-30 22:03:15 +0100

    SI: Changes: 1. Removed the WashU setting module 2. Changed liquibase bean name 3. Fetching the CPs in a chunk rather than one-by-one. 4. Other minor changes

--- washu/src/main/java/com/krishagni/openspecimen/washu/jobs/CpCoordinatorsReportTask.java
+++ washu/src/main/java/com/krishagni/openspecimen/washu/jobs/CpCoordinatorsReportTask.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
@@ -19,8 +18,8 @@ import com.krishagni.catissueplus.core.common.util.ConfigUtil;
 import com.krishagni.catissueplus.core.common.util.EmailUtil;
 
 @Configurable
-public class ScheduledCPCoordinatorsReport implements ScheduledTask {
-	private final static String WASHU_CP_COORD_REPORT = "washu_cp_coord_report";
+public class CpCoordinatorsReportTask implements ScheduledTask {
+	private final static String CP_COORD_REPORT = "washu_cp_coord_report";
 
 	@Autowired
 	private DaoFactory daoFactory;
@@ -28,18 +27,34 @@ public class ScheduledCPCoordinatorsReport implements ScheduledTask {
 	@Override
 	public void doJob(ScheduledJobRun job) throws Exception {
 		List<Long> cpIds = getAllCpIds();
-		for (Long cpId : cpIds) {
-			notifyPiForCp(cpId);
-		}
+		int startAt = 0;
+		int maxResults = 25;
+
+		processCpIds(cpIds, startAt, maxResults);
 	}
 
 	@PlusTransactional
-	private void notifyPiForCp(Long cpId) {
-		CollectionProtocol cp = getCpById(cpId);
+	private void processCpIds(List<Long> cpIds, int startAt, int maxResults) {
+		while (true) {
+			List<CollectionProtocol> cps = getCpByIds(cpIds.stream().skip(startAt).limit(maxResults).collect(Collectors.toList()));
+
+			for (CollectionProtocol cp : cps) {
+				notifyPiForCp(cp);
+			}
+
+			if (cps.size() != maxResults) {
+				break;
+			}
+
+			startAt += maxResults;
+		}
+	}
+
+	private void notifyPiForCp(CollectionProtocol cp) {
 		String piEmail = cp.getPrincipalInvestigator().getEmailAddress();
 		Map<String, Object> mailProps = getProps(cp);
 
-		EmailUtil.getInstance().sendEmail(WASHU_CP_COORD_REPORT, new String[] { piEmail }, null, mailProps);
+		EmailUtil.getInstance().sendEmail(CP_COORD_REPORT, new String[] { piEmail }, null, mailProps);
 	}
 
 	@PlusTransactional
@@ -47,20 +62,18 @@ public class ScheduledCPCoordinatorsReport implements ScheduledTask {
 		return daoFactory.getCollectionProtocolDao().getAllCpIds();
 	}
 
-	private CollectionProtocol getCpById(Long cpId) {
-		return daoFactory.getCollectionProtocolDao().getById(cpId);
+	private List<CollectionProtocol> getCpByIds(List<Long> cpIds) {
+		return daoFactory.getCollectionProtocolDao().getByIds(cpIds);
 	}
 
 	private Map<String, Object> getProps(CollectionProtocol cp) {
 		Map<String, Object> props = new HashMap<>();
-		String helpdeskEmail = ConfigUtil.getInstance().getStrSetting("washu", "washu_helpdesk_email", "");
-		String contactEmail = ConfigUtil.getInstance().getStrSetting("washu", "washu_contact_email", "");
 		String openspecimenAccEmail = ConfigUtil.getInstance().getStrSetting("email", "account_id", "");
 
 		props.put("cpShortTitle", cp.getShortTitle());
 		props.put("cpCoords", cp.getCoordinators().stream().map(User::formattedName).collect(Collectors.joining(", ")));
-		props.put("helpdeskEmail", StringUtils.isEmpty(helpdeskEmail) ? openspecimenAccEmail : helpdeskEmail);
-		props.put("contactEmail", StringUtils.isEmpty(contactEmail) ? openspecimenAccEmail : contactEmail);
+		props.put("helpdeskEmail", openspecimenAccEmail);
+		props.put("contactEmail", openspecimenAccEmail);
 		return props;
 	}
 }
