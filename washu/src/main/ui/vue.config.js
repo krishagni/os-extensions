0cd0cdb459c1511b1c34315081a4e539e7a89d63
Vinayak Pawar <vinayakapawar@gmail.com>
2025-05-13 20:06:53 +0530

    Fixed the WU plugin

--- washu/src/main/ui/vue.config.js
+++ washu/src/main/ui/vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin("copy")
-      .use(require.resolve("copy-webpack-plugin"), [[{
+      .use(require.resolve("copy-webpack-plugin"), [{patterns: [{
         from: "src/i18n",
         to: "i18n"
-      }]]);
+      }]}]);
   }
 }
