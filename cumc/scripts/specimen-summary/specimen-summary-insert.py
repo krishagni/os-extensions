0f74e7e02bd1ec44867bc14ef7670528eeaa2860
Krishagni-build <build@krishagni.com>
2025-09-24 09:23:08 +0000

    1. Added multiselect clinical diagnosis and sequencing type & results

--- cumc/scripts/specimen-summary/specimen-summary-insert.py
+++ cumc/scripts/specimen-summary/specimen-summary-insert.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 )
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Truncate and insert into visit_specimen_type_summary')
+    parser = argparse.ArgumentParser(description='Truncate and insert into visit_specimen_type_summary and visit_summary')
     parser.add_argument('config_file', help='Path to DB config JSON file')
     return parser.parse_args()
 
@@ -39,54 +39,147 @@ def get_db_connection(config):
         sys.exit(1)
 
 def truncate_and_insert(conn):
-    truncate_query = "TRUNCATE TABLE visit_specimen_type_summary"
-    
-    insert_query = """
+    truncate_visit_summary = "TRUNCATE TABLE visit_summary"
+    truncate_visit_specimen_type_summary = "TRUNCATE TABLE visit_specimen_type_summary"
+    truncate_sequencing_type_and_results_summary = "TRUNCATE TABLE sequencing_type_and_results_summary"
+    insert_query1 = """
     INSERT INTO visit_specimen_type_summary (
         visit_id,
         specimen_type,
-        count_of_aliquots,
-        sequencing_type,
-        sequencing_result
+        count_of_aliquots
     )
     SELECT
         spmn.specimen_collection_group_id AS visit_id,
         spmn_type.value AS specimen_type,
-        COUNT(DISTINCT spmn.identifier) AS count_of_aliquots,
-        de.DE_A_2 as sequencing_type,
-        de.DE_A_5 as sequncing_result
+        COUNT(*) AS count_of_aliquots
     FROM
         catissue_specimen spmn
         LEFT JOIN catissue_coll_event_param coll_event ON coll_event.specimen_id = spmn.identifier
         LEFT JOIN catissue_permissible_value spmn_type ON spmn_type.identifier = spmn.specimen_type_id
-        left join (
-            OS_SPMN_EXTN_RECS form
-            inner join DE_E_11151 de on de.IDENTIFIER = form.RECORD_ID
-            and form.FORM_ID = 226
-    ) on form.SPECIMEN_ID = spmn.IDENTIFIER
+        LEFT JOIN (
+            OS_SPMN_EXTN_RECS form_rec
+            INNER JOIN OS_CHILD_SPMN_EVENTS child_events ON child_events.IDENTIFIER = form_rec.record_id
+            AND form_rec.FORM_ID = 75
+        ) ON form_rec.SPECIMEN_ID = spmn.IDENTIFIER
     WHERE
-        spmn.lineage = 'Aliquot'
-        AND spmn.activity_status != 'Disabled'
+        spmn.activity_status != 'Disabled'
         AND spmn_type.value IS NOT NULL
         AND spmn.availability_status = 'Available'
         AND spmn.collection_status = 'Collected'
+    AND (
+         ( spmn.lineage = 'New' 
+           AND child_events.IDENTIFIER IS NULL
+         )
+       )
     GROUP BY
-        spmn.specimen_collection_group_id,
-        spmn_type.value,
-        sequencing_type,
-        sequncing_result
+      spmn.specimen_collection_group_id,
+      spmn_type.value
+    """
+
+    insert_query2 = """
+    INSERT INTO visit_specimen_type_summary (
+        visit_id,
+        specimen_type,
+        count_of_aliquots
+    )
+    SELECT
+    spmn.specimen_collection_group_id AS visit_id,
+    spmn_type.value AS specimen_type,
+    COUNT(*) AS count_of_aliquots
+FROM
+    catissue_specimen spmn
+    LEFT JOIN catissue_coll_event_param coll_event ON coll_event.specimen_id = spmn.identifier
+    LEFT JOIN catissue_permissible_value spmn_type ON spmn_type.identifier = spmn.specimen_type_id
+WHERE
+    spmn.activity_status != 'Disabled'
+    AND spmn_type.value IS NOT NULL
+    AND spmn.availability_status = 'Available'
+    AND spmn.collection_status = 'Collected'
+    AND spmn.lineage = 'Aliquot'
+GROUP BY
+    spmn.specimen_collection_group_id,
+    spmn_type.value
+    """
+
+    update_query = """
+    UPDATE visit_specimen_type_summary vsts
+    JOIN CATISSUE_SPECIMEN_COLL_GROUP scg
+      ON vsts.VISIT_ID = scg.IDENTIFIER
+    JOIN OS_CPR_RECS cpr
+      ON scg.COLLECTION_PROTOCOL_REG_ID = cpr.CPR_ID
+    SET vsts.COLLECTION_AGE = TIMESTAMPDIFF(YEAR, cpr.DOB, scg.COLLECTION_TIMESTAMP)
+    WHERE scg.ACTIVITY_STATUS != 'Disabled'
+      AND cpr.ACTIVITY_STATUS != 'Disabled';
+    """
+
+    insert_query3 = """
+    INSERT INTO visit_summary (
+        SPEC_TYPE_SUMMARY_ID,
+        VISIT_CUSTOM_CLINICAL_DIAGNOSIS
+    )
+    SELECT DISTINCT
+        vsts.IDENTIFIER AS SPEC_TYPE_SUMMARY_ID,
+        de.DE_A_3 AS VISIT_CUSTOM_CLINICAL_DIAGNOSIS
+    FROM catissue_specimen_coll_group visit
+    JOIN visit_specimen_type_summary vsts
+      ON vsts.VISIT_ID = visit.IDENTIFIER
+    LEFT JOIN os_visit_extn_recs visit_ext
+      ON visit_ext.VISIT_ID = visit.IDENTIFIER
+      AND visit_ext.FORM_ID = 928
+    LEFT JOIN de_e_12200 de
+      ON de.IDENTIFIER = visit_ext.RECORD_ID
+    WHERE visit.activity_status = 'Active'
+      AND de.DE_A_3 IS NOT NULL;
     """
     
+    insert_query4="""
+    INSERT INTO sequencing_type_and_results_summary (
+    SPEC_TYPE_SUMMARY_ID,
+    SEQUENCING_TYPE_AND_RESULTS
+)
+SELECT
+    vsts.IDENTIFIER AS SPEC_TYPE_SUMMARY_ID,
+    CONCAT(de.DE_A_2, ' (', de.DE_A_5, ')') AS SEQUENCING_TYPE_AND_RESULTS
+FROM de_e_11151 de
+JOIN os_spmn_extn_recs form
+  ON form.RECORD_ID = de.IDENTIFIER
+  AND form.FORM_ID = 226
+JOIN catissue_specimen spmn
+  ON spmn.IDENTIFIER = form.SPECIMEN_ID
+LEFT JOIN visit_specimen_type_summary vsts
+  ON vsts.VISIT_ID = spmn.SPECIMEN_COLLECTION_GROUP_ID
+WHERE spmn.activity_status != 'Disabled'
+  AND spmn.availability_status = 'Available'
+  AND spmn.collection_status = 'Collected'
+  AND (de.DE_A_2 IS NOT NULL OR de.DE_A_5 IS NOT NULL);
+    """
+
     try:
         with conn.cursor() as cursor:
+            logging.info("Disabling foreign key checks...")
+            cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
+
             logging.info("Executing TRUNCATE...")
-            cursor.execute(truncate_query)
+            cursor.execute(truncate_sequencing_type_and_results_summary)
+            cursor.execute(truncate_visit_summary)
+            cursor.execute(truncate_visit_specimen_type_summary)
             logging.info("TRUNCATE complete.")
 
-            logging.info("Executing INSERT...")
-            cursor.execute(insert_query)
+            logging.info("Re-enabling foreign key checks...")
+            cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
+
+            logging.info("Executing INSERT into visit_specimen_type_summary...")
+            cursor.execute(insert_query1)
+            cursor.execute(insert_query2)
+            cursor.execute(insert_query3)
+            cursor.execute(insert_query4)
+            logging.info("Updating COLLECTION_AGE...")
+            cursor.execute(update_query)
+            logging.info("Inserting into visit_summary...")
+            cursor.execute(insert_query3)
+
             conn.commit()
-            logging.info("INSERT complete. Records inserted.")
+            logging.info("All operations completed successfully.")
     except mysql.connector.Error as err:
         conn.rollback()
         logging.error(f"Query execution failed: {err}")
--- cumc/scripts/specimen-summary/tables.sql
+++ cumc/scripts/specimen-summary/tables.sql
@@ -1,79 +1,33 @@
+CREATE TABLE `visit_summary` (
+  `IDENTIFIER` bigint NOT NULL AUTO_INCREMENT,
+  `SPEC_TYPE_SUMMARY_ID` bigint NOT NULL,
+  `VISIT_CUSTOM_CLINICAL_DIAGNOSIS` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`IDENTIFIER`),
+  KEY `IDX_VISIT_SUMMARY_SPEC_TYPE_SUMMARY_ID` (`SPEC_TYPE_SUMMARY_ID`),
+  KEY `IDX_VISIT_SUMMARY_DIAGNOSIS` (`VISIT_CUSTOM_CLINICAL_DIAGNOSIS`),
+  CONSTRAINT `FK_VISIT_SUMMARY_SPEC_TYPE_SUMMARY` FOREIGN KEY (`SPEC_TYPE_SUMMARY_ID`) REFERENCES `visit_specimen_type_summary` (`IDENTIFIER`)
+)
+
 CREATE TABLE `visit_specimen_type_summary` (
   `IDENTIFIER` bigint NOT NULL AUTO_INCREMENT,
   `VISIT_ID` bigint NOT NULL,
   `SPECIMEN_TYPE` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
   `COUNT_OF_ALIQUOTS` int DEFAULT NULL,
-  `SEQUENCING_TYPE` varchar(255) DEFAULT NULL,
-  `SEQUENCING_RESULT` varchar(255) DEFAULT NULL,
+  `collection_age` int DEFAULT NULL,
   PRIMARY KEY (`IDENTIFIER`),
   KEY `IDX_VSTS_VISIT_ID` (`VISIT_ID`),
   KEY `IDX_VSTS_SPECIMEN_TYPE` (`SPECIMEN_TYPE`),
   KEY `IDX_VSTS_COUNT_OF_ALIQUOTS` (`COUNT_OF_ALIQUOTS`),
-  KEY `IDX_VSTS_SEQUENCING_TYPE` (`SEQUENCING_TYPE`),
-  KEY `IDX_VSTS_SEQUENCING_RESULT` (`SEQUENCING_RESULT`),
+  KEY `IDX_VSTS_COLLECTION_AGE` (`collection_age`),
   CONSTRAINT `FK_VSTS_VISIT_ID` FOREIGN KEY (`VISIT_ID`) REFERENCES `catissue_specimen_coll_group` (`IDENTIFIER`)
 )
 
-INSERT INTO visit_specimen_type_summary (
-        visit_id,
-        specimen_type,
-        count_of_aliquots,
-        sequencing_type,
-        sequencing_result
-    )
-SELECT
-    spmn.specimen_collection_group_id AS visit_id,
-    spmn_type.value AS specimen_type,
-    COUNT(DISTINCT spmn.identifier) AS count_of_aliquots,
-    de.DE_A_2 as sequencing_type,
-    de.DE_A_5 as sequncing_result
-FROM
-    catissue_specimen spmn
-    LEFT JOIN catissue_coll_event_param coll_event ON coll_event.specimen_id = spmn.identifier
-    LEFT JOIN catissue_permissible_value spmn_type ON spmn_type.identifier = spmn.specimen_type_id
-    left join (
-        OS_SPMN_EXTN_RECS form
-        inner join DE_E_11151 de on de.IDENTIFIER = form.RECORD_ID
-        and form.FORM_ID = 226
-    ) on form.SPECIMEN_ID = spmn.IDENTIFIER
-WHERE
-    spmn.lineage = 'Aliquot'
-    AND spmn.activity_status != 'Disabled'
-    AND spmn_type.value IS NOT NULL
-    AND spmn.availability_status = 'Available'
-    AND spmn.collection_status = 'Collected'
-GROUP BY
-    spmn.specimen_collection_group_id,
-    spmn_type.value,
-    sequencing_type,
-    sequncing_result
-
-ALTER TABLE visit_specimen_type_summary
-ADD COLUMN collection_age INT DEFAULT NULL;
-
-CREATE INDEX IDX_VSTS_COLLECTION_AGE ON visit_specimen_type_summary (collection_age);
-
-SELECT
-    vsts.IDENTIFIER AS SUMMARY_ID,
-    vsts.VISIT_ID,
-    cpr.DOB,
-    scg.COLLECTION_TIMESTAMP,
-    TIMESTAMPDIFF(YEAR, cpr.DOB, scg.COLLECTION_TIMESTAMP) AS COLLECTION_AGE
-FROM visit_specimen_type_summary vsts
-JOIN CATISSUE_SPECIMEN_COLL_GROUP scg
-  ON vsts.VISIT_ID = scg.IDENTIFIER
-JOIN OS_CPR_RECS cpr
-  ON scg.COLLECTION_PROTOCOL_REG_ID = cpr.CPR_ID
-WHERE scg.ACTIVITY_STATUS != 'Disabled'
-  AND cpr.ACTIVITY_STATUS != 'Disabled';
-
-
-UPDATE visit_specimen_type_summary vsts
-JOIN CATISSUE_SPECIMEN_COLL_GROUP scg
-  ON vsts.VISIT_ID = scg.IDENTIFIER
-JOIN OS_CPR_RECS cpr
-  ON scg.COLLECTION_PROTOCOL_REG_ID = cpr.CPR_ID
-SET vsts.COLLECTION_AGE = TIMESTAMPDIFF(YEAR, cpr.DOB, scg.COLLECTION_TIMESTAMP)
-WHERE scg.ACTIVITY_STATUS != 'Disabled'
-  AND cpr.ACTIVITY_STATUS != 'Disabled';
-
+CREATE TABLE `sequencing_type_and_results_summary` (
+  `IDENTIFIER` bigint NOT NULL AUTO_INCREMENT,
+  `SPEC_TYPE_SUMMARY_ID` bigint NOT NULL,
+  `SEQUENCING_TYPE_AND_RESULTS` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`IDENTIFIER`),
+  KEY `IDX_STARS_SPEC_TYPE_SUMMARY_ID` (`SPEC_TYPE_SUMMARY_ID`),
+  KEY `IDX_STARS_TYPE_AND_RESULTS` (`SEQUENCING_TYPE_AND_RESULTS`),
+  CONSTRAINT `FK_STARS_SPEC_TYPE_SUMMARY` FOREIGN KEY (`SPEC_TYPE_SUMMARY_ID`) REFERENCES `visit_specimen_type_summary` (`IDENTIFIER`)
+)
