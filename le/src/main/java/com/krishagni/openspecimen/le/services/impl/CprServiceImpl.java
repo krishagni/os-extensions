41c0242bc9c1ef98861fa15a68bc4fdb4e02d0b5
Richard Bramley <rabramley@gmail.com>
2025-11-13 11:43:50 +0000

    Add debugging

--- le/src/main/java/com/krishagni/openspecimen/le/services/impl/CprServiceImpl.java
+++ le/src/main/java/com/krishagni/openspecimen/le/services/impl/CprServiceImpl.java
@@ -29,7 +29,7 @@ import com.krishagni.openspecimen.le.services.CprService;
 
 public class CprServiceImpl implements CprService {
 	
-	private static final Log logger = Logger.getLogger(AsigServiceImpl.class);
+	private static final Log logger = Logger.getLogger(CprServiceImpl.class);
 	private DaoFactory daoFactory;
 	
 	private LabelGenerator labelGenerator;
